9822203959f3a694e20511ad3bd6909cad4e107e
Mayank <134766594+x-imayank@users.noreply.github.com>
2024-01-07 01:06:11 +0530

    Create CP_setup.cpp

---
+++ CP_setup.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+void solve(); 
+int main() 
+{ 
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL); 
+  
+#ifndef ONLINE_JUDGE 
+    freopen("input.txt", "r", stdin); 
+    freopen("error.txt", "w", stderr); 
+    freopen("output.txt", "w", stdout); 
+#endif 
+  
+    int t = 1; 
+    cin >> t; 
+    while (t--) { 
+        solve(); 
+        cout << "\n"; 
+    } 
+  
+    cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl; 
+    return 0; 
+} 
+void solve() 
+{ 
+} 
